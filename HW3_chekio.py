9da8ac9691385e4ac06e3239e944004e347e7af8
xomaiya <xomaiya@gmail.com>
2019-10-01 11:37:50 +0300

    Fix comments

--- HW3_chekio.py
+++ HW3_chekio.py
@@ -1,7 +1,2 @@
-# from collections import Counter
-
-
 def chekio(ls):
-    # counter = Counter(ls)
-    # return [el for el in ls if counter[el] > 1]
     return [el for el in ls if ls.count(el) > 1]
